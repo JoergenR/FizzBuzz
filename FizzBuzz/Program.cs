4c66abfd0f8f7d9bc5365c2923c29385112bdb6d
Jørgen Ringstad <36072991+JoergenR@users.noreply.github.com>
2019-02-07 13:18:51 +0100

    Now writes the result to a console window

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,16 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            int number = 100;
+
+            for (int i = 1; i <= number; i++)
+            {
+                var result = FizzBuzz.GetFizzBuzz(i);
+
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
         }
     }
 }
