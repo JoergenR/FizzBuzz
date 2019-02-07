9c845d8d57ad07acc97ac54d71e0242e4133fb3c
Jørgen Ringstad <36072991+JoergenR@users.noreply.github.com>
2019-02-07 12:43:27 +0100

    Added FizzBuzz.UnitTests and created the tests that need to be passed. Also added the FizzBuzz class and created the GetFizzBuzz method. All test passed

---
+++ FizzBuzz.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzz.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetFizzBuzz_InputIsDividableBy3And5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetFizzBuzz(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        
+        [Test]
+        public void GetFizzBuzz_InputIsDividableBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetFizzBuzz(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetFizzBuzz_InputIsDividableBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetFizzBuzz(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetFizzBuzz_InputIsNotDividableBy3Or5_ReturnSameNumber()
+        {
+            var result = FizzBuzz.GetFizzBuzz(1);
+
+            Assert.That(result, Is.EqualTo("1"));
+        }
+
+
+    }
+}
